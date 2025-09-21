062a84a09f54e7a964c9015c990b4b5e431204a5
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-09-21 18:19:12 +0300

    Add validation Order

--- server/middlewares/validationSchema.js
+++ server/middlewares/validationSchema.js
@@ -65,3 +65,21 @@ module.exports.REVIEW_VALIDATION_SCHEMA = yup.object({
   comment: yup.string().trim().required('Field comment is required'),
   date: yup.date().max(new Date(), 'Date cannot be in future'),
 });
+
+module.exports.ORDER_VALIDATION_SCHEMA = yup.object({
+  customerId: yup.number().integer().required('Field customer id is required'),
+  products: yup.array({
+    productId: yup.number().integer().required('Field product id is required'),
+    amount: yup
+      .number()
+      .min(0, 'Amount cannot be negative')
+      .integer('Amount must be integer')
+      .required('Field amount is required'),
+  }),
+  totalSumma: yup.number().min(0).required('Field total summa is required'),
+  status: yup
+    .string()
+    .oneOf(['new', 'processing', 'shipped', 'delivered'])
+    .required('Field status is required'),
+  date: yup.date().max(new Date(), 'Date cannot be in future'),
+});
