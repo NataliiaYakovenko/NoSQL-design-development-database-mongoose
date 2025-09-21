079e9ee666806eb5ba00744e96d1afff3b51fc7b
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-09-21 17:48:59 +0300

    Add validation for Review

--- server/middlewares/validationSchema.js
+++ server/middlewares/validationSchema.js
@@ -53,3 +53,15 @@ module.exports.CUSTOMER_VALIDATION_SCHEMA = yup.object({
   deliveryAddress: yup.string().required('Field delivery address is required'),
   wishlist: yup.array(),
 });
+
+module.exports.REVIEW_VALIDATION_SCHEMA = yup.object({
+  productId: yup.number().integer().required('Field product id is required'),
+  customerId: yup.number().integer().required('Field customer id is required'),
+  mark: yup
+    .number()
+    .min(1, 'Mark cannot be less then 1')
+    .max(10, 'Mark cannot be more then 10')
+    .required('Field mark is required'),
+  comment: yup.string().trim().required('Field comment is required'),
+  date: yup.date().max(new Date(), 'Date cannot be in future'),
+});
