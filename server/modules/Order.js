90fadcb8c6dcbbf40d7dc513cf6da66c8eb81278
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-09-21 18:02:01 +0300

    Add model Orer

--- server/modules/Order.js
+++ server/modules/Order.js
@@ -1,8 +1,54 @@
 const mongoose = require('mongoose');
 
-const orderSchema = new mongoose.Schema({
-
-});
+const orderSchema = new mongoose.Schema(
+  {
+    customerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Customer',
+      required: true,
+    },
+    products: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Product',
+          required: true,
+        },
+        amount: {
+          type: Number,
+          required: true,
+          min: 1,
+          validation: {
+            validate: Number.isInteger,
+            message: 'Amount must be in integer',
+          },
+        },
+      },
+    ],
+    totalSumma: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    status: {
+      type: String,
+      enum: ['new', 'processing', 'shipped', 'delivered'],
+      default: 'new',
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+      validate: {
+        validator: (value) => value <= new Date(),
+        message: 'Date cannot be in future',
+      },
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
 
 const Order = mongoose.model('Order', orderSchema);
 module.exports = Order;
